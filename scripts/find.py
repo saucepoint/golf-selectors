de186599bf8456a5d684ae31413e7c6457619bd4
saucepoint <saucepoint@protonmail.com>
2022-05-22 15:53:11 -0400

    multiprocessing optimization

--- scripts/find.py
+++ scripts/find.py
@@ -4,27 +4,17 @@ import string
 from itertools import permutations
 import random
 import math
+from multiprocessing import Process, Queue
+import time
 
-def find():
-    with open('vars.txt', 'r') as f:
-        vars = [var.strip() for var in f.readlines()]
 
-    # load in 10,000 common google searched words
-    with open('words.txt', 'r') as f:
-        words = [word.strip() for word in f.readlines()]
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
 
-    # construct camelCase permutations of word pairs
-    # i.e. words = [dog, cat], combos = [dogDog, catCat, dogCat, catDog]
-    combos = [f'{word[0]}{word[1].capitalize()}' for word in permutations(random.sample(words, 2000), 2)]
-    
-    # 3 letter function names will also be considered
-    alphabet = list(string.ascii_lowercase)
-    triplets = [''.join(perm) for perm in permutations(alphabet, 3)]
 
-    # construct a global list of function names to brute force through
-    function_names = words + combos + triplets
-    print(f"Searching through {len(function_names)} function names")
-
+def find_selector(function_names, q):
     # brute force search through the function names
     # identify which function name produces the function selector
     # with the most number of leading zeros
@@ -43,8 +33,52 @@ def find():
             min_selector = selector_int
             best_name = function_name
             best_selector = selector
-    print(f'{best_name} ({best_selector})')
     
+    q.put({
+        'name': best_name,
+        'selector': best_selector,
+        'int': min_selector
+    })
+
+def find():
+    # load in 10,000 common google searched words
+    with open('words.txt', 'r') as f:
+        words = [word.strip() for word in f.readlines()]
+
+    # construct camelCase permutations of word pairs
+    # i.e. words = [dog, cat], combos = [dogDog, catCat, dogCat, catDog]
+    combos = [f'{word[0]}{word[1].capitalize()}' for word in permutations(random.sample(words, 2000), 2)]
+    
+    # 3 letter function names will also be considered
+    alphabet = list(string.ascii_lowercase)
+    triplets = [''.join(perm) for perm in permutations(alphabet, 3)]
+
+    # construct a global list of function names to brute force through
+    function_names = words + combos + triplets
+    print(f"Searching through {len(function_names)} function names")
+
+    # split the brute force search across multiple processes
+    jobs = []
+    q = Queue()
+    for chunk in chunks(function_names, 500_000):
+        p = Process(target=find_selector, args=(chunk, q))
+        p.start()
+        jobs.append(p)
+        time.sleep(0.1)
+    
+    # iterate the results to determine the best
+    min_selector = math.inf
+    best_name = None
+    best_selector = None
+    for job in jobs:
+        job.join()
+        result = q.get()
+        if result['int'] < min_selector:
+            min_selector = result['int']
+            best_name = result['name']
+            best_selector = result['selector']
+
+    print(f'{best_name} ({best_selector})')
 
 if __name__ == '__main__':
     find()
